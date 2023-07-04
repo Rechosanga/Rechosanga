1f455c02563e20cfaad23489dbc5701dad8429cc
Rechosanga <sangab7@gmail.com>
2023-07-03 17:36:33 -0700

    Update ST_Transformer.py

--- ST_Transformer.py
+++ ST_Transformer.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 28 10:28:06 2020
-
-@author: wb
-"""
 
 import torch
 import torch.nn as nn
@@ -38,16 +32,13 @@ class SSelfAttention(nn.Module):
         values  = self.values(values)  # (N, T, heads, head_dim)
         keys    = self.keys(keys)      # (N, T, heads, head_dim)
         queries = self.queries(query)  # (N, T, heads, heads_dim)
-
         # Einsum does matrix mult. for query*keys for each training example
         # with every other training example, don't be confused by einsum
         # it's just how I like doing matrix multiplication & bmm
-
         energy = torch.einsum("qthd,kthd->qkth", [queries, keys])   # space  self-attention
         # queries shape: (N, T, heads, heads_dim),
         # keys shape: (N, T, heads, heads_dim)
         # energy: (N, N, T, heads)
-
         # Normalize energy values similarly to seq2seq + attention
         # so that they sum to 1. Also divide by scaling factor for
         # better stability
@@ -61,13 +52,10 @@ class SSelfAttention(nn.Module):
         # values shape: (N, T, heads, heads_dim)
         # out after matrix multiply: (N, T, heads, head_dim), then
         # we reshape and flatten the last two dimensions.
-
         out = self.fc_out(out)
         # Linear layer doesn't modify the shape, final shape will be
         # (N, T, embed_size)
-
         return out
-    
 class TSelfAttention(nn.Module):
     def __init__(self, embed_size, heads):
         super(TSelfAttention, self).__init__()
@@ -88,15 +76,12 @@ class TSelfAttention(nn.Module):
         N, T, C = query.shape
 
         # Split the embedding into self.heads different pieces
-        values = values.reshape(N, T, self.heads, self.head_dim)  # embed_size Dimensions disassembled into    
-                                                                  #heads×head_dim
+        values = values.reshape(N, T, self.heads, self.head_dim)  # embed_size Dimensions disassembled into     #heads×head_dim                                          
         keys   = keys.reshape(N, T, self.heads, self.head_dim)
         query  = query.reshape(N, T, self.heads, self.head_dim)
-
         values  = self.values(values)  # (N, T, heads, head_dim)
         keys    = self.keys(keys)      # (N, T, heads, head_dim)
         queries = self.queries(query)  # (N, T, heads, heads_dim)
-
         # Einsum does matrix mult. for query*keys for each training example
         # with every other training example, don't be confused by einsum
         # it's just how I like doing matrix multiplication & bmm
@@ -104,15 +89,11 @@ class TSelfAttention(nn.Module):
         # queries shape: (N, T, heads, heads_dim),
         # keys shape: (N, T, heads, heads_dim)
         # energy: (N, T, T, heads)
-        
-        
         # Normalize energy values similarly to seq2seq + attention
         # so that they sum to 1. Also divide by scaling factor for
         # better stability
-        attention = torch.softmax(energy / (self.embed_size ** (1 / 2)), dim=2)  # Do softmax in K dimension, and 
-                                                                                 #1
+        attention = torch.softmax(energy / (self.embed_size ** (1 / 2)), dim=2)  # Do softmax in K dimension, and  #1                                                   
         # attention shape: (N, query_len, key_len, heads)
-
         out = torch.einsum("nqkh,nkhd->nqhd", [attention, values]).reshape(
                 N, T, self.heads * self.head_dim
         )
@@ -120,14 +101,10 @@ class TSelfAttention(nn.Module):
         # values shape: (N, T, heads, heads_dim)
         # out after matrix multiply: (N, T, heads, head_dim), then
         # we reshape and flatten the last two dimensions.
-
         out = self.fc_out(out)
         # Linear layer doesn't modify the shape, final shape will be
         # (N, T, embed_size)
-
         return out
-    
-    
 class STransformer(nn.Module):
     def __init__(self, embed_size, heads, adj, dropout, forward_expansion):
         super(STransformer, self).__init__()
@@ -135,46 +112,37 @@ class STransformer(nn.Module):
         self.adj = adj
         self.D_S = nn.Parameter(adj)
         self.embed_liner = nn.Linear(adj.shape[0], embed_size)
-        
         self.attention = SSelfAttention(embed_size, heads)
         self.norm1 = nn.LayerNorm(embed_size)
         self.norm2 = nn.LayerNorm(embed_size)
-
         self.feed_forward = nn.Sequential(
             nn.Linear(embed_size, forward_expansion * embed_size),
             nn.ReLU(),
             nn.Linear(forward_expansion * embed_size, embed_size),
         )
-        
         # transfer  GCN
         self.gcn = GCN(embed_size, embed_size*2, embed_size, dropout)  
         self.norm_adj = nn.InstanceNorm2d(1)    # Normalize the adjacency matrix
-
         self.dropout = nn.Dropout(dropout)
         self.fs = nn.Linear(embed_size, embed_size)
         self.fg = nn.Linear(embed_size, embed_size)
-
-    def forward(self, value, key, query):
-                
+    def forward(self, value, key, query):       
         # Spatial Embedding part
         N, T, C = query.shape
         D_S = self.embed_liner(self.D_S)
         D_S = D_S.expand(T, N, C)
         D_S = D_S.permute(1, 0, 2)
-        
-        
+    
         # GCN part
         X_G = torch.Tensor(query.shape[0], 0, query.shape[2])
         self.adj = self.adj.unsqueeze(0).unsqueeze(0)
         self.adj = self.norm_adj(self.adj)
         self.adj = self.adj.squeeze(0).squeeze(0)
-        
         for t in range(query.shape[1]):
             o = self.gcn(query[ : , t,  : ],  self.adj)
             o = o.unsqueeze(1)              # shape [N, 1, C]
             X_G = torch.cat((X_G, o), dim=1)
 
-        
         # Spatial Transformer part
         query = query+D_S
         attention = self.attention(value, key, query)
@@ -183,7 +151,6 @@ class STransformer(nn.Module):
         forward = self.feed_forward(x)
         U_S = self.dropout(self.norm2(forward + x))
 
-        
         # fusion STransformer and GCN
         g = torch.sigmoid( self.fs(U_S) +  self.fg(X_G) )      # (7)
         out = g*U_S + (1-g)*X_G                                # (8)
@@ -198,9 +165,6 @@ class TTransformer(nn.Module):
         self.time_num = time_num
         self.one_hot = One_hot_encoder(embed_size, time_num)          # temporal embedding select one-hotway or
         self.temporal_embedding = nn.Embedding(time_num, embed_size)  # temporal embedding select nn.Embedding
-
-
-        
         self.attention = TSelfAttention(embed_size, heads)
         self.norm1 = nn.LayerNorm(embed_size)
         self.norm2 = nn.LayerNorm(embed_size)
@@ -211,7 +175,6 @@ class TTransformer(nn.Module):
             nn.Linear(forward_expansion * embed_size, embed_size),
         )
         self.dropout = nn.Dropout(dropout)
-
     def forward(self, value, key, query, t):
         N, T, C = query.shape
         
@@ -219,19 +182,14 @@ class TTransformer(nn.Module):
         D_T = self.temporal_embedding(torch.arange(0, T))    # temporal embedding select nn.Embedding
         D_T = D_T.expand(N, T, C)
 
-
         # temporal Add embedding to query。 The original paper uses concatenated
         query = query + D_T  
-        
         attention = self.attention(value, key, query)
-
         # Add skip connection, run through normalization and finally dropout
         x = self.dropout(self.norm1(attention + query))
         forward = self.feed_forward(x)
         out = self.dropout(self.norm2(forward + x))
         return out
-
-
 class STTransformerBlock(nn.Module):
     def __init__(self, embed_size, heads, adj, time_num, dropout, forward_expansion):
         super(STTransformerBlock, self).__init__()
@@ -240,13 +198,11 @@ class STTransformerBlock(nn.Module):
         self.norm1 = nn.LayerNorm(embed_size)
         self.norm2 = nn.LayerNorm(embed_size)
         self.dropout = nn.Dropout(dropout)
-    
     def forward(self, value, key, query, t):
         # Add skip connection,run through normalization and finally dropout
         x1 = self.norm1(self.STransformer(value, key, query) + query)
         x2 = self.dropout( self.norm2(self.TTransformer(x1, x1, x1, t) + x1) )
         return x2
-
 class Encoder(nn.Module):
     # stack multiple layers ST-Transformer Block
     def __init__(
@@ -286,7 +242,6 @@ class Encoder(nn.Module):
         for layer in self.layers:
             out = layer(out, out, out, t)
         return out     
-    
 class Transformer(nn.Module):
     def __init__(
         self,
@@ -315,8 +270,6 @@ class Transformer(nn.Module):
     def forward(self, src, t):
         enc_src = self.encoder(src, t)
         return enc_src
-
-
 class STTransformer(nn.Module):
     def __init__(
         self, 
@@ -337,45 +290,28 @@ class STTransformer(nn.Module):
             embed_size, 
             num_layers, 
             heads, 
-            time_num
-        )
-                
+            time_num)    
         # # Reduce the time dimension. Example: T_dim=12 to output_T_dim=3, input 12 dimensions down to output 3    
         #dimensions
         self.conv2 = nn.Conv2d(T_dim, output_T_dim, 1)  
         # Reduce the number of channels and reduce to 1 dimension
         self.conv3 = nn.Conv2d(embed_size, 1, 1)
         self.relu = nn.ReLU()
-    
     def forward(self, x, t):
         # input x shape[ C, N, T] 
                 #C: number of channels. N: Number of sensors. T: amount of time
-
-        
         x = x.unsqueeze(0)
         input_Transformer = self.conv1(x)        
         input_Transformer = input_Transformer.squeeze(0)
         input_Transformer = input_Transformer.permute(1, 2, 0)  
-        
         #input_Transformer shape[N, T, C]
         output_Transformer = self.Transformer(input_Transformer, t)  
         output_Transformer = output_Transformer.permute(1, 0, 2)
         #output_Transformer shape[T, N, C]
-        
         output_Transformer = output_Transformer.unsqueeze(0)     
         out = self.relu(self.conv2(output_Transformer))    # 等号左边 out shape: [1, output_T_dim, N, C]        
         out = out.permute(0, 3, 2, 1) #left of the equal sign out shape: [1, C, N, output_T_dim]
         out = self.conv3(out)            # left of the equal signout shape: [1, 1, N, output_T_dim]       
         out = out.squeeze(0).squeeze(0)
-        
-       
         return out
         # return out shape: [N, output_dim]
-    
-
-
-    
-
-    
-    
-    
